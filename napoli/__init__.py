cd2b1291f569fbfde2f7381f22104d89413b9568
Luigi Noto <luiginoto@Luigis-MBP.cable.rcn.com>
2022-04-26 19:46:19 -0400

    Made some changes

--- napoli/__init__.py
+++
@@ -1,5 +0,0 @@
-print(f'Invoking (custom) __init__.py for {__name__}')
-from .ratingsSplit import ratingsSplit
-
-# https://stackoverflow.com/questions/12172791/changes-in-import-statement-python3
-# https://timothybramlett.com/How_to_create_a_Python_Package_with___init__py.html
