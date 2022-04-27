7070b9132135b1353beafb58cef8b25cf59fac98
Giacomo Bugli <GIacomoBugli@10-16-162-244.dynapool.wireless.nyu.edu>
2022-04-27 14:25:45 -0400

    working and efficient version

--- popularity/popularity.py
+++ popularity/popularity.py
@@ -8,32 +8,37 @@ from pyspark.mllib.evaluation import RankingMetrics
 
 
 class PopularityBaseline():
-
+    
     def __init__(self, threshold = None, damping = 0):
-
+        
         self.threshold = threshold
         self.damping = damping
         self.fitted = False
         self.results = None
-
+        
+    def __repr__(self):
+        return 'PopularityBaseline(threshold = ' + str(self.threshold) + ', damping = ' + str(self.damping) + ', fitted = ' + str(self.fitted) + ')'
+        
     def fit(self, ratings, top_n=100):
         ratings = ratings.groupBy('movieId').agg(fn.sum('rating').alias('tot_rating'), fn.count('userId').alias('counts')) \
             .withColumn('counts', fn.col('counts') + self.damping)
         if self.threshold is not None:
             ratings = ratings.filter(ratings.counts >= self.threshold)
         results = ratings.withColumn('popularity', fn.col('tot_rating') / fn.col('counts')).drop('tot', 'counts').orderBy(fn.desc('popularity'))
-        results = results.limit(top_n).select('movieId').rdd.flatMap(lambda x: x).collect()
+        results = results.limit(top_n).select('movieId')
         self.fitted = True
         self.results = results
         return results
-
+    
     def evaluate(self, results, test_set):
         data = test_set.filter(test_set.rating > 2.5).drop('rating','timestap')
-        UserMovies=[]
-        users = [x.userId for x in test_set.select('userId').distinct().collect()]
-        for user in users:
-            UserMovies.append((results, data.filter('userId'==user).select('movieId').rdd.flatMap(lambda x: x).collect()))
-        predictionAndLabels = spark.sparkContext.parallelize(UserMovies)
+        
+        results = results.withColumn('id', fn.lit(1)).groupBy('id').agg(fn.collect_list('movieId').alias('top_movies'))
+        
+        UserMovies = data.groupBy('userId').agg(fn.collect_list('movieId').alias('liked_movies'))
+        UserMovies = UserMovies.withColumn('id', fn.lit(1))
+        UserMovies = UserMovies.join(results, 'id').select('top_movies', 'liked_movies')
+        predictionAndLabels = UserMovies.rdd.map(tuple)
         metrics = RankingMetrics(predictionAndLabels)
         return metrics
 
