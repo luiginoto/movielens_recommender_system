d1769d1dea5a19dbb81ddbfab5945156217b7d2d
Guilherme Albertini <gcalbertini@gmail.com>
2022-05-15 04:54:22 -0400

    Delete to start anew

--- validated_models/__init__.py
+++
@@ -1,5 +0,0 @@
-print(f'Invoking __init__.py for {__name__}')
-from .popularity import PopularityBaseline
-from .ALS import CustomALS
-from .popularity_validation import PopularityBaselineValidation
-from .als_validation import ALSValidation
